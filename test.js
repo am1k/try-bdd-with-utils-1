4380b8e77d06b65e229add33b6fb8e689bcbb35d
am1k <vitaliy_bog@mail.ru>
2015-07-08 12:36:15 +0300

    add new tests

--- test.js
+++ test.js
@@ -1,22 +1,171 @@
 var utils = require('./utils'),
-    expect =  require('expect.js');
+	expect =  require('expect.js');
 
 describe('Utils', function() {
 
 	describe('#sort()', function() {
 		it('should sort given array of numbers with ascending ordering', function() {
-			expect(utils.sort([2, 1, 3, 0])).to.equal([0, 1, 2, 3]);
+			expect(utils.sort([2, 1, 3, 0]).join()).to.equal([0, 1, 2, 3].join());
 		});
 
-		//TODO: don't forget about custom comparator function
+		it('should sort given array of numbers with ascending ordering', function() {
+			expect(utils.sort([2, 1, 3, 0], function(a, b){return a > b}).join()).to.equal([0, 1, 2, 3].join());
+		});
+
 	});
 
 	describe('#capitalize()', function() {
 		it('should make first letter of given string upper case', function() {
 			expect(utils.capitalize('just do it!')).to.equal('Just do it!');
 		});
+
+		it('should make first letter of given string upper case', function() {
+			expect(utils.capitalize('JUST DO IT')).to.equal('Just do it');
+		});
+
+		it('should make first letter of given string upper case', function() {
+			expect(utils.capitalize('jUST DO IT')).to.equal('Just do it');
+		});
+
 	});
 
-	//TODO: Add your specs for other methods
+	describe('#camelize()', function() {
+		it('should make all first letter to upper case', function() {
+			expect(utils.camelize('Equipment Class name')).to.equal('EquipmentClassName');
+		});
 
+		it('should make all first letter to upper case', function() {
+			expect(utils.camelize('Equipment class Name')).to.equal('EquipmentClassName');
+		});
+
+		it('should make all first letter to upper case', function() {
+			expect(utils.camelize('equipment class name')).to.equal('EquipmentClassName');
+		});
+
+		it('should make all first letter to upper case', function() {
+			expect(utils.camelize('Equipment Class Name')).to.equal('EquipmentClassName');
+		});
+
+		it('should make all first letter to upper case', function() {
+			expect(utils.camelize(['Equipment', 'Class', 'Name'])).to.equal('EquipmentClassName');
+		});
+
+	});
+
+
+	describe('#trim()', function() {
+		it('should make one word', function() {
+			expect(utils.trim(' just  do  it ')).to.equal('justdoit');
+		});
+
+		it('should make one word', function() {
+			expect(utils.trim(' just  do  it          ')).to.equal('justdoit');
+		});
+
+		it('should make one word', function() {
+			expect(utils.trim('         just  do  it          ')).to.equal('justdoit');
+		});
+
+	});
+	describe('#reverse()', function() {
+		it ('should show word reverse', function() {
+			expect(utils.reverse(['just', 'do', 'it']).join()).to.equal(['it', 'do', 'just'].join());
+		})
+	});
+
+	describe('#map()', function () {
+		it('should change each list element by applying handler', function () {
+			var newArr = [10, 20, 30, 40, 50];
+
+			expect(utils.map(newArr, function (int) {
+				return --int;
+			}).join()).to.equal([9, 19, 29, 39, 49].join());
+		});
+
+		it('should change each list element by applying handler', function () {
+			var firstObj = {
+				firstName: 'Vitaliy',
+				lastName: 'Bog',
+				address: 'india, deli, ololoeva str, 4b',
+				age: 35
+			};
+
+			var secondObj = {
+				firstName: 'VITALIY',
+				lastName: 'BOG',
+				address: 'INDIA, DELI, OLOLOEVA STR, 4B',
+				age: 35
+			};
+
+			expect(utils.map(firstObj, function (string) {
+				string += '';
+				return string.toUpperCase();
+			}).toString()).to.equal(secondObj.toString());
+		});
+	});
+
+	describe('#groupBy()',  function () {
+		it('Should accept array with single element and return according object', function () {
+			var testArray = [1.1,1.2,2.1,2.3];
+
+			expect(utils.groupBy(testArray, function (num) {
+				return Math.floor(num);
+			})).to.eql({'1': [ 1.1, 1.2 ], '2': [ 2.1, 2.3 ]});
+		});
+
+		it('Should accept array with single element and return according object', function () {
+			var testArray = [1.1,1.2,2.1,2.3];
+
+			expect(utils.groupBy(testArray, function (num) {
+				return Math.floor(num);
+			})).to.eql({'1': [ 1.1, 1.2 ], '2': [ 2.1, 2.3 ]});
+		});
+	});
+
+	describe('#once()', function () {
+		it('function that can only be called one time', function () {
+			var count = 0;
+			var newFunction = utils.once(function(){count++});
+			newFunction();
+			newFunction();
+			expect(count).to.equal(1);
+		})
+	});
+
+	describe('#debounce()', function () {
+		it('new debounced version of the passed function', function () {
+			var count = 1000;
+			var text = 'Hello';
+			var newFunction = utils.debounce(function() {return text.toUpperCase()}, count);
+			newFunction();
+			newFunction();
+			newFunction();
+			newFunction();
+			console.log(newFunction);
+			expect(text.toUpperCase()).to.equal('HELLO');
+		});
+	});
+
+	describe('#deepEqual()', function () {
+		it ('This is the same object', function () {
+			var first = {2: [2,3]};
+			var second = {2: [2,3]};
+			expect(utils.deepEqual(second,first)).to.equal(true);
+		});
+		it('different types', function () {
+			var first = [2,4];
+			var second = {3: 5};
+			expect(utils.deepEqual(first,second)).to.equal(false);
+		});
+		it('different arrays', function () {
+			var first = [2,4,6];
+			var second = [2,5,7];
+			expect(utils.deepEqual(first,second)).to.equal(false);
+		});
+		it('different objects', function () {
+			var first = {2: 4};
+			var second = {2: 5};
+			expect(utils.deepEqual(first,second)).to.equal(false);
+		});
+	});
 });
--- utils.js
+++ utils.js
@@ -1,43 +1,75 @@
 module.exports = {
- 
+
     /**
      * Sort given array by provided rule in comparator function
      * @param {Array} list
      * @param {Function} comparator
      */
- 
+
     sort:function (list, comparator) {
-        return [];
+        var count = list.length;
+        for (var i = 0; i < count-1; i++)
+        { for (var j = 0; j < count-1-i; j++)
+        { if ((comparator && comparator(list[j], list[j + 1])) ||
+            (!comparator && list[j] > list[j+1]))
+        { var app = list[j+1];
+            list[j+1] = list[j];
+            list[j] = app; }
+        }
+        }
+        return list;
     },
- 
+
     /**
      * Make first letter of given string upper case
      * @param {String} string
      * @return {String} capitalized string
      */
- 
+
     capitalize:function (string) {
-        return "";
+        var lowerCase = string.toLowerCase();
+        return lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
     },
- 
+
     /**
      * Camelize given string or array of string
      * @param {Array|String} sequence
      * @return {String} capitalized string
      */
- 
+
     camelize:function (sequence) {
-        return "";
+        var string = '';
+
+        if (this.isArray(sequence)) {
+            for (var i = 0; i < sequence.length; i++) {
+                if ((typeof sequence[i]) === 'object') {
+                    var newArr = sequence[i];
+                    string += module.exports.camelize(newArr);
+                }
+                else {
+                    sequence[i] = sequence[i].replace(/\W/, '');
+                    sequence[i] = sequence[i].replace(/[0-9]]/);
+
+                    string += module.exports.capitalize(sequence[i]);
+                }
+            }
+        }
+        else if (this.isString(sequence)) {
+            var newArr = sequence.split(' ');
+            string += module.exports.camelize(newArr);
+        }
+        return string;
     },
- 
+
+
     /**
      * Cut of any count of spaces from the beginning and from the end of the string
      * @param {String} str
      * @return {String}
      */
- 
+
     trim:function (str) {
-        return "";
+        return str.replace(/ +/g,"");
     },
 
     /**
@@ -45,56 +77,154 @@ module.exports = {
      * @param {Array} list - a list to be reversed, may be empty.
      * @return {Array} - the same instance of list but reverted
      */
- 
+
     reverse:function (list) {
-        return [];
+        for (var i = 0; i < list.length / 2; i++) {
+            var newArr = list[i];
+            list[i] = list[list.length - 1 - i];
+            list[list.length - 1 - i] = newArr;
+        }
+        return list;
     },
- 
+
     /**
      *  Change each list's element by applying handler
      *  @params {Array|Object} list - input sequence
      *  @params {Function} iterator  - some rule which changes each element
      *  @return {Array} new list with changes elements
      */
- 
-    map:function (list, iterator) {
-        return [];
+
+
+    map: function (list, iterator) {
+        if (this.isObject(list)) {
+            var newObj = {};
+
+            for (var someProperty in list) {
+                if (list.hasOwnProperty(someProperty)) {
+                    newObj[someProperty] = iterator(list[someProperty]);
+                }
+            }
+
+            return newObj;
+        } else if (this.isArray(list)) {
+            var newArr = [];
+
+            for (var i = 0; i < list.length; i++) {
+                newArr.push(iterator(list[i]));
+            }
+            return newArr;
+        }
     },
- 
+
+
     /**
      * Group some input sequence of element by some rule
      * @param {Array} list - input sequence
      * @param {Function} iterator -  provide group id for each element
      * @return {Object} object of group id properties which point to arrays of element from input sequence
      */
- 
+
     groupBy:function (list, iterator) {
-        return {};
+        var results = {};
+        for (var i = 0; i < list.length; i++) {
+            var key = iterator(list[i]);
+            if (results.hasOwnProperty(key)) {
+                results[key].push(list[i]);
+            } else {
+                results[key] = [list[i]];
+            }
+        }
+        return results;
     },
 
+
     /**
-     * Creates a version of the function that can only be called one time. 
-     * Repeated calls to the modified function will have no effect. 
+     * Creates a version of the function that can only be called one time.
+     * Repeated calls to the modified function will have no effect.
      * @param {Function} func - your target function
      * @return {Function} new  function which could be invoked only once
      */
 
     once: function(func){
-        return;
-    }, 
-
+        var executed = false;
+        return function() {
+            if (!executed) {
+                executed = true;
+                return func.apply(this, arguments)
+            }
+        }
+    },
 
     /**
-     * Creates and returns a new debounced version of the passed function 
-     * which will postpone its execution until after wait milliseconds 
-     * have elapsed since the last time it was invoked. 
+     * Creates and returns a new debounced version of the passed function
+     * which will postpone its execution until after wait milliseconds
+     * have elapsed since the last time it was invoked.
      * @param {Function} func - your target function
      * @param {Number} wait -  milliseconds have elapsed since the last time it was invoked
      * @return {Function} new debounced version of the passed function
      */
 
     debounce: function(func, wait){
-        return;
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                func.apply(context, args);
+            };
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    },
+
+    deepEqual: function(listOne, listTwo) {
+        var arrays = this.isArray(listOne) && this.isArray(listTwo);
+        var objects = (!this.isArray(listOne) &&  typeof listOne === 'object') && (!this.isArray(listTwo) &&  typeof listTwo === 'object');
+        var firstObjKeys = objects ? Object.keys(listOne) : listOne;
+        var secondObjKeys = objects ? Object.keys(listTwo) : listTwo;
+        var currentKey;
+
+        if ((!arrays && !objects) || (firstObjKeys.length !== secondObjKeys.length)) {
+            return false;
+        }
+        if (objects) {
+            for (var i = 0; i < firstObjKeys.length; i++) {
+                currentKey = firstObjKeys[i];
+                if(typeof listOne[currentKey] === 'object' && typeof listTwo[currentKey] === 'object'){
+                    if(!this.deepEqual(listOne[currentKey], listTwo[currentKey])){
+                        return false;
+                    }
+                }else if (secondObjKeys.indexOf(currentKey) == -1 || listOne[currentKey] !== listTwo[currentKey]) {
+                    //console.log('simple equal')
+                    return false;
+                }
+            }
+        } else if (arrays) {
+            for (var i = 0; i < listOne.length; i++) {
+
+                if(typeof listOne[i] === 'object' && typeof listTwo[i] === 'object'){
+                    if(!this.deepEqual(listOne[i], listTwo[i])){
+                        return false;
+                    }
+                }else if(listOne[i] !== listTwo[i]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    },
+
+    isArray: function(obj) {
+        return Object.prototype.toString.call(obj).toUpperCase() === '[OBJECT ARRAY]';
+    },
+
+    isObject: function(obj) {
+        return Object.prototype.toString.call(obj).toUpperCase() === '[OBJECT OBJECT]';
+    },
+
+    isString: function(obj) {
+        return Object.prototype.toString.call(obj).toUpperCase() === '[OBJECT STRING]';
     }
- 
+
 };
